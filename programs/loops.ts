a8ecdb9ca5ab57987afbd66ebc6579b0e40554d9
bhushantbn <bhushantbn@gmail.com>
2025-03-01 14:49:09 +0530

    added loops program

---
+++ programs/loops.ts
@@ -0,0 +1,9 @@
+const fruits:string[]=["apple","banana","WaterMelon"]
+
+for(let i =0; i<fruits.length;i++){
+    if(fruits[i]==="banana"){
+       fruits[i]="grapes"
+    }
+    console.log(fruits[i]);
+}
+    
